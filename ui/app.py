55b418dda4997c92e2e4d164da5b670f00e2760b
Liz <etche@plattsburgh.edu>
2024-09-12 14:37:00 -0400

    implemented flask to buttons

--- ui/app.py
+++ ui/app.py
@@ -6,9 +6,27 @@ app = Flask(__name__)
 def home():
     return render_template('app.html')
 
+@app.route('/alerts')
+def alers():
+    return render_template('app.html')
+
+@app.route('/grafana')
+def grafana():
+    return render_template('app.html')
+
+@app.route('/logs')
+def logs():
+    return render_template('app.html')
+
+@app.route('/incoming_traffic')
+def incoming_traffic():
+    return render_template('app.html')
+
 @app.route('/login')
 def login_page():
     return render_template('Loginpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
