547211a619af48e5262498ca6c8b29534b56eb33
etche001 <etche001@plattsburgh.edu>
2024-09-24 15:27:49 -0400

    added test data to logs

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tech Startup Project: Beekeepers
-Google Drive: https://drive.google.com/drive/folders/1GbiOosbB_9vXJZrTG6XXQaHW68Ce4Foy?usp=drive_link
--- ui/app.py
+++ ui/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import json
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('app.html')
+
+@app.route('/logs')
+def logs():
+    # Get the absolute path to the JSON file
+    file_path = os.path.join(os.path.dirname(__file__), 'test_data.json')
+
+    try:
+         with open(file_path, 'r') as file:
+            logs_data = json.load(file)
+    except FileNotFoundError:
+        logs_data = []  # In case the file is not found, return an empty list
+
+    # Pass the logs to the logs.html template
+    return render_template('logs.html', logs=logs_data)
 
 @app.route('/alerts')
 def alerts():
@@ -13,10 +29,6 @@ def alerts():
 @app.route('/grafana')
 def grafana():
     return render_template('grafana.html')
-
-@app.route('/logs')
-def logs():
-    return render_template('logs.html')
 
 @app.route('/incoming-traffic')
 def incoming_traffic():
@@ -28,5 +40,4 @@ def login_page():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
