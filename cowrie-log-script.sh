25c260ccea2249173f2af8bce9b7d0738d676d4e
cjbates02 <christianbates02@gmail.com>
2024-10-10 16:59:20 -0400

    logs config adjustment

--- cowrie-log-script.sh
+++ cowrie-log-script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 while true; do
-    filename=$(ls /var/log/containers/ | grep pf-pot)
+    filename=$(ls /var/log/containers/ | grep cowrie)
+    formatted_date=$(date +"%Y-%m-%d %H:%M:%S") 
     if [[ -z "$filename" ]]; then
         sleep 5
         continue
     fi
     
-    cp /var/log/containers/"$filename" /persistent-volumes/honeypot-logs/cowrie.log
+    sudo cp /var/log/containers/"$filename" /persistent-volumes/honeypot-logs/cowrie.log
+    sudo chmod o+r /persistent-volumes/honeypot-logs/cowrie.log
     sleep 5
 done
