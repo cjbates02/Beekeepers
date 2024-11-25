5b003ec9f5bc3093a3d61e7670903e21490f4682
cjbates02 <christianbates02@gmail.com>
2024-11-24 23:52:03 -0500

    calculate utilization percentages

--- ui/core/prom_client.py
+++ ui/core/prom_client.py
@@ -9,7 +9,7 @@ load_dotenv()
 QUERY_CPU_USAGE = 'sum(rate(container_cpu_usage_seconds_total[1m]))%20by%20(pod)'
 QUERY_CPU_LIMIT = 'sum(kube_pod_container_resource_requests{resource="cpu"})%20by%20(pod)'
 QUERY_MEMORY_USAGE = 'sum(container_memory_usage_bytes)%20by%20(pod)'
-QUERY_MEMORY_LIMIT = 'sum(kube_pod_container_resource_limits%7Bresource%3D"memory"%7D)%20by%20(pod)'
+QUERY_MEMORY_LIMIT = 'sum(kube_pod_container_resource_requests{resource="memory"})%20by%20(pod)'
 QUERY_STATUS = 'kube_pod_status_phase{pod=~".*"}'
 
 class PromClient:
@@ -34,7 +34,14 @@ class PromClient:
             pod = metric['metric']['pod']
             value = metric['value'][1]
             if self.processed_data.get(pod, None) == None:
-                self.processed_data[pod] = {'cpu_usage': None, 'cpu_limit': None, 'memory_usage': None, 'memory_limit': None, 'status': None}
+                self.processed_data[pod] = {'cpu_usage': None, 
+                                            'cpu_limit': None, 
+                                            'memory_usage': None, 
+                                            'memory_limit': None, 
+                                            'status': None,
+                                            'cpu_usage_percentage': None,
+                                            'memory_usage_percentage': None}
+            
             self.processed_data[pod][metric_name] = value
         
         pprint(self.processed_data)
@@ -46,9 +53,22 @@ class PromClient:
         self.process_query(QUERY_MEMORY_USAGE, 'memory_usage')
         self.process_query(QUERY_MEMORY_LIMIT, 'memory_limit')
         self.process_query(QUERY_STATUS, 'status')
-        pprint(self.processed_data)
+    
+    
+    def get_utilization_percentages(self):
+        self.calculate_utilization('cpu_usage', 'cpu_limit', 'cpu_usage_percentage')
+        self.calculate_utilization('memory_usage', 'memory_limit', 'memory_usage_percentage')
+    
         
+    def calculate_utilization(self, metric_usage, metric_limit, metric_usage_percentage_name):
+        for pod in self.processed_data:
+            if self.processed_data[pod][metric_usage] and self.processed_data[pod][metric_limit]:
+                self.processed_data[pod][metric_usage_percentage_name] = round(float(self.processed_data[pod][metric_usage]) / float(self.processed_data[pod][metric_limit]), 2) * 100
+
+
 
 if __name__ == '__main__':
     prom_client = PromClient()
     prom_client.process_all_queries()
+    prom_client.get_utilization_percentages()
+    pprint(prom_client.processed_data)
