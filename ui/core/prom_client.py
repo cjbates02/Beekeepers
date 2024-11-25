adfef8763ac9cef5605a3ba6b52e9c8dd3cb45ce
cjbates02 <christianbates02@gmail.com>
2024-11-24 23:24:21 -0500

    completed version 1 of the prom client. pulling hardware limits and usage as well as pod status

--- ui/core/prom_client.py
+++ ui/core/prom_client.py
@@ -2,11 +2,12 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
+from pprint import pprint
 
 load_dotenv()
 
 QUERY_CPU_USAGE = 'sum(rate(container_cpu_usage_seconds_total[1m]))%20by%20(pod)'
-QUERY_CPU_LIMIT = 'sum(kube_pod_container_resource_limits%7Bresource%3D"cpu"%7D)%20by%20(pod)'
+QUERY_CPU_LIMIT = 'sum(kube_pod_container_resource_requests{resource="cpu"})%20by%20(pod)'
 QUERY_MEMORY_USAGE = 'sum(container_memory_usage_bytes)%20by%20(pod)'
 QUERY_MEMORY_LIMIT = 'sum(kube_pod_container_resource_limits%7Bresource%3D"memory"%7D)%20by%20(pod)'
 QUERY_STATUS = 'kube_pod_status_phase{pod=~".*"}'
@@ -14,6 +15,8 @@ QUERY_STATUS = 'kube_pod_status_phase{pod=~".*"}'
 class PromClient:
     def __init__(self):
         self.api_url = os.getenv('PROM_API_URL')
+        self.processed_data = {}
+    
     
     def execute_query(self, query):
         response = requests.get(self.api_url + query)
@@ -23,30 +26,29 @@ class PromClient:
         else:
             print(f'Prom query {query} failed with status code {response.status_code}.')
             return None
-
-    
-    def get_pod_cpu_metrics(self):
-        cpu_usage_metrics = self.execute_query(QUERY_CPU_USAGE)
-        cpu_limit_metrics = self.execute_query(QUERY_CPU_LIMIT)
-        print(cpu_usage_metrics, '\n')
-        print(cpu_limit_metrics)
-    
-    def get_pod_memory_metrics():
-        pass
-    
-    
-    def process_hardware_metrics():
-        pass
+        
+        
+    def process_query(self, query, metric_name):
+        metrics = self.execute_query(query)
+        for metric in metrics['data']['result']:
+            pod = metric['metric']['pod']
+            value = metric['value'][1]
+            if self.processed_data.get(pod, None) == None:
+                self.processed_data[pod] = {'cpu_usage': None, 'cpu_limit': None, 'memory_usage': None, 'memory_limit': None, 'status': None}
+            self.processed_data[pod][metric_name] = value
+        
+        pprint(self.processed_data)
     
     
-    def get_pod_status_metrics():
-        pass
-    
-    
-    def process_state_metrics():
-        pass
-    
+    def process_all_queries(self):
+        self.process_query(QUERY_CPU_USAGE, 'cpu_usage')
+        self.process_query(QUERY_CPU_LIMIT, 'cpu_limit')
+        self.process_query(QUERY_MEMORY_USAGE, 'memory_usage')
+        self.process_query(QUERY_MEMORY_LIMIT, 'memory_limit')
+        self.process_query(QUERY_STATUS, 'status')
+        pprint(self.processed_data)
+        
 
 if __name__ == '__main__':
     prom_client = PromClient()
-    prom_client.get_pod_cpu_metrics()
+    prom_client.process_all_queries()
